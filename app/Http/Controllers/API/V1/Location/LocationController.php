1ef780d4fe0c34f20a84523f6156955af22b42c6
olegsolovjn@gmail.com <vodugu46>
2022-06-21 16:39:31 +0300

    swagger fix

--- app/Http/Controllers/API/V1/Location/LocationController.php
+++ app/Http/Controllers/API/V1/Location/LocationController.php
@@ -5,7 +5,29 @@ namespace App\Http\Controllers\API\V1\Location;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Get(
+ *     path="/location_list",
+ *     summary="Updates a user",
+ *     @OA\Parameter(
+ *         description="Parameter with mutliple examples",
+ *         in="path",
+ *         name="id",
+ *         required=true,
+ *         @OA\Schema(type="string"),
+ *         @OA\Examples(example="int", value="1", summary="An int value."),
+ *         @OA\Examples(example="uuid", value="0006faf6-7a61-426c-9034-579f2cfcfa83", summary="An UUID value."),
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     )
+ * )
+ */
+
 class LocationController extends Controller
 {
-    //
+    public function index(){
+        return 1;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/location_list',[\App\Http\Controllers\API\V1\Location\LocationController::class,'index']);
